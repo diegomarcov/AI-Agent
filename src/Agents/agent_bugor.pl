fac720546139a80bfb44a06375eecc8cafb8e6e9
diegomarcov <diegomarcov@gmail.com>
2010-09-12 19:14:46 -0300

    Cambiado el agente para que funcione en modo joystick. Ahora ya ve bien las posadas, y no explota al caminar.

--- src/Agents/agent_bugor.pl
+++ src/Agents/agent_bugor.pl
@@ -12,19 +12,20 @@
 :- dynamic agentes/1.
 
 turn(0).
+posadas([]).
 
 run:-
       get_percept(Perc),
-      
-	  write(Perc), nl,
+      % write('This is perceptiooooooooooooooon!'), nl, nl,
+	  % write(Perc), nl,
 	  
       update_state(Perc),
 
-      %decide_action(Action),
+      % decide_action(Action),
 	  ag_name(AgName),
       
       display_ag(AgName, Perc), nl,
-      
+      % agregado esto para que el agente actue en modo joystick
       write('ACCION?: '), read(Action),
       
 	  do_action(Action),
@@ -34,12 +35,12 @@ run:-
 % Auxiliares %%%%%%%%%%%%%%%%%%%%%
 
 % Reemplaza conocimiento guardado con asserts
-replace(X, Y):- retractall(X), !,
+replace(X, Y):- write('Trying to retract....'), nl,nl, retractall(X), !,
                 assert(Y).
 
 % Caso especial en que el retractall falla 
 % porque no existe el conocimiento X
-replace(X, Y):- assert(Y).
+replace(X, Y):- write('Retract failed! Asserting only...'),nl,nl,assert(Y).
 
 % Utilizado para "recordar" cosas solo una
 % vez
@@ -61,7 +62,11 @@ update_state([Turn, Vision, Attr, Inventory]):- save_map(Vision),
 
 % El primer ObjectsAtSight recibido es:
 % [[[8,7],[agent,bugor,[[dir,n],[unconscious,false],[previous_turn_action,none]]]],[[5,7],[treasure,t1,[[val,100]]]],[[7,9],[treasure,t4,[[val,100]]]]]
-save_map(Vision):- write('Getting objects at sight...'), nl, objects_at_sight(Vision, ObjectsAtSight), write(Vision), nl, nl, nl, write(ObjectsAtSight),
+
+% con esta vision se rompe todo:
+% [4,[[[5,6],water,[]],[[4,6],water,[]],[[3,6],plain,[]],[[2,6],plain,[]],[[5,5],plain,[]],[[4,5],water,[]],[[3,5],plain,[]],[[2,5],mountain,[]],[[5,7],plain,[[agent,bugor,[[dir,n],[unconscious,false],[previous_turn_action,move_fwd]]],[treasure,t1,[[val,100]]]]],[[4,7],plain,[]],[[3,7],plain,[]],[[2,7],plain,[]],[[5,8],plain,[]],[[4,8],plain,[]],[[3,8],forest,[]],[[2,8],forest,[]],[[5,9],plain,[]],[[4,9],forest,[]],[[3,9],plain,[]],[[2,9],plain,[[hostel,h2,[]],[treasure,t5,[[val,100]]]]]],[[pos,[5,7]],[dir,n],[stamina,147],[max_stamina,150],[fight_skill,100]],[]]
+
+save_map(Vision):- objects_at_sight(Vision, ObjectsAtSight),
 					forall(member([Pos, Obj], ObjectsAtSight), (analize_things([Pos, Obj]))), write('Done with forall'), nl.
 
 % Analiza un elemento visto
@@ -69,11 +74,22 @@ save_map(Vision):- write('Getting objects at sight...'), nl, objects_at_sight(Vi
 % la posicion
 %
 % NOTA: Las posadas parecen tener Type=hostel, Attr=[].
+% este es el caso en el que pasa por un hotel que ya conoce
+analize_things([Pos, Obj]):- write('I am currently analizing this: '),nl,nl, write(Obj),nl,nl,
+							Obj = [Type, Name, Attrs],
+							Type = hostel, nl, nl, write('Ho visto a Hostel'), nl, nl,
+							posadas(P),
+							member(Pos, P),
+							% Agrego la posada a la lista
+							write('Ya conozco este hostel! Mi lista de posadas actual es:'), nl, write([Pos|P]), nl, nl.
+							
+% en este caso, el agente ve un hotel nuevo							
 analize_things([Pos, Obj]):- Obj = [Type, Name, Attrs],
-							Type = hostel,
+							Type = hostel, nl, nl, write('Ho visto a Hostel'), nl, nl,
 							posadas(P),
 							% Agrego la posada a la lista
-							replace(posadas(_), posadas([Pos | P])).
+							replace(posadas(_), posadas([Pos | P])),
+							write('Hostel added! Mi lista de posadas actual es:'), nl, write([Pos|P]), nl, nl.
 
 % NOTA: Los tesoros tienen como atributo el valor
 % pero parece ser siempre 100.
@@ -94,6 +110,9 @@ analize_things([Pos, Obj]):- Obj = [Type, Name, Attrs],
 							% Para todos los agentes: se lo recuerda y se lo asigna a una zona
 							forall(member([AtrName, Value], Attrs), (remember_agent(Name, [AttrName, Value]), zone_it(Name, Pos))).
 
+							
+% este caso es cuando bugor se ve a si mismo; simplemente se ignora
+% otra opcion: imprimir un simpatico cartel que diga "HELLO! IT'S-A ME, BUUUUGOOOOR!"
 analize_things([Pos, Obj]):- Obj = [Type, Name, Attrs],
 							Type = agent.
 % Predicado para filtrar atributos de agente
