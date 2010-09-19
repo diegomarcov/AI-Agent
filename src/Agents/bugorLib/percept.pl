dc53b82e1724c0c772901970a3c6d2efa1a715cc
diegomarcov <diegomarcov@gmail.com>
2010-09-19 12:59:18 -0300

    Agregado UNO DE LOS peteS al olvidar el oro

--- src/Agents/bugorLib/percept.pl
+++ src/Agents/bugorLib/percept.pl
@@ -1,26 +1,34 @@
 % Percept %%%%%%%%%%%%%%%%%%%%%%%%
 
-update_state([Turn, Vision, _Attr, _Inventory]):- 
-	save_turn(Turn), 
+update_state([Turn, Vision, _Attr, _Inventory]):-
+	save_turn(Turn),
 	save_map(Vision).
-% TODO: 
+% TODO:
 %	- Ver que se guarde bien el mapa
 
 processPosition(X, Y, Land, Objects):-
-	write('NKLA PUTA AMDRE'), nl,
+%	write('Estoy en el primer caso para la posicion '), write(X), write(','), write(Y), nl, nl,
 	assert_once(map(X, Y, Land)),  % Guardamos el mapa
 	oro(X,Y),
 	not (member([treasure,_,_], Objects)),
 	retract(oro(X,Y)).
-%	write('Current objects at position: '), write(X), write(','), write(Y), nl, nl, write(Objects), nl, nl.
+%	write('Current objects at position: '), write(X), write(','), write(Y),
+%       nl, nl, write(Objects), nl, nl.
 
+% si falla, es o bien porque no habia oro en esa posicion, o porque el oro esta y ya era recordado,
+% asi que unicamente guardo el mapa.
+processPosition(X, Y, Land, Objects):-
+%	write('Estoy en el segundo caso para la posicion '), write(X), write(','), write(Y), nl, nl,
+	assert_once(map(X, Y, Land)).  % Guardamos el mapa
+	
+	
 % Mapa %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Recorre todos los elementos a la vista
 % y los analiza por separado
-save_map(Vision):- 
+save_map(Vision):-
 	write('CRISTO'), nl,
-	forall(member([[X, Y], Land, Objects], Vision), processPosition(X,Y,Land,Objects)), % En este predicado guardamos el mapa y borramos el oro recordado en caso de que ya no este
+	forall(member([[X, Y], Land, Objects], Vision), processPosition(X, Y, Land, Objects)), % Guardo el mapa y borro el oro
 	findall([X, Y, Land], map(X, Y, Land), Mapa),
 	term_to_atom(Mapa, M),
 	debug_term(info, 'Known Map: ', M),
@@ -33,7 +41,7 @@ save_map(Vision):-
 %
 % NOTA: Las posadas parecen tener Type=hostel, Attr=[].
 % este es el caso en el que pasa por un hotel que ya conoce
-analize_things([Pos, Obj]):- 
+analize_things([Pos, Obj]):-
 	debug_term(info, 'Currently analizing this: ', Obj),
 	Obj = [hostel, _Name, _Attrs],
 	posadas(P),
@@ -41,7 +49,7 @@ analize_things([Pos, Obj]):-
 	debug_term(info, 'Already known. Known hostels: ', P).
 
 % en este caso, el agente ve un hotel nuevo
-analize_things([Pos, Obj]):- 
+analize_things([Pos, Obj]):-
 	debug_term(info, 'Currently analizing this: ', Obj),
 	Obj = [hostel, _Name, _Attrs],
 	posadas(P),
@@ -51,7 +59,7 @@ analize_things([Pos, Obj]):-
 
 % NOTA: Los tesoros tienen como atributo el valor
 % pero parece ser siempre 100.
-analize_things([Pos, Obj]):- 
+analize_things([Pos, Obj]):-
 	debug_term(info, 'Currently analizing this: ', Obj),
 	Obj = [treasure, _Name, _Attrs],
 	% Agrego el tesoro
@@ -60,12 +68,12 @@ analize_things([Pos, Obj]):-
 	findall(X, oro(X, _Y), O),
 	debug_term(info, 'Known treasures: ', O).
 
-% NOTAS: 
+% NOTAS:
 % - Recordar ver si el agente es uno mismo % - En los atributos:
 %    - previous_turn_action = {none, attack(name), turn(orientation), pickup(objname), move_fwd}
 %    - unconcious = {true, false}
 %    - dir = {n, s, w, e}
-analize_things([_Pos, Obj]):- 
+analize_things([_Pos, Obj]):-
 	debug_term(info, 'Currently analizing this: ', Obj),
 	Obj = [agent, Name, Attrs],
 	Name \= bugor, % Descartamos analizar este mismo agente
@@ -73,14 +81,14 @@ analize_things([_Pos, Obj]):-
 	forall(member([AttrName, Value], Attrs), remember_agent(Name, [AttrName, Value])).
 
 % este caso es cuando bugor se ve a si mismo; simplemente se ignora
-analize_things([_Pos, Obj]):- 
+analize_things([_Pos, Obj]):-
 	Obj = [agent, bugor, _Attrs],
 	debug(warning,'HELLO! IT´S-A ME, BUUUUGOOOOR!').
 
 % Predicado para filtrar atributos de agente
 %
 % Si se lo vio atacando a otro, y ya conocemos a este agente:
-remember_agent(Name, [previous_turn_action, attack(_)]):- 
+remember_agent(Name, [previous_turn_action, attack(_)]):-
 	debug(info, 'remember_agent: Case A: The agent is seeing attacking and it´s already known.'),
 	agentes(A),
 	debug_term(info, 'Known agents: ', A),
@@ -94,7 +102,7 @@ remember_agent(Name, [previous_turn_action, attack(_)]):-
 	debug_term(info, 'Updated agent list: ', VerA).
 
 % Si el predicado member falla, es decir, nunca vimos a este agente:
-remember_agent(Name, [previous_turn_action, attack(_)]):- 
+remember_agent(Name, [previous_turn_action, attack(_)]):-
 	debug(info, 'remember_agent: Case B: The agent is seeing attacking but it´s the first time we saw it.'),
 	agentes(A),
 	debug_term(info, 'Known agents: ', A),
@@ -103,7 +111,7 @@ remember_agent(Name, [previous_turn_action, attack(_)]):-
 	debug_term(info, 'Updated agent list: ', VerA).
 
 % Analogamente para los tesoros recojidos
-remember_agent(Name, [previous_turn_action, pickup(_)]):- 
+remember_agent(Name, [previous_turn_action, pickup(_)]):-
 	debug(info, 'remember_agent: Case C: The agent is seeing picking a treasure and it´s already known.'),
 	agentes(A),
 	debug_term(info, 'Known agents: ', A),
@@ -117,7 +125,7 @@ remember_agent(Name, [previous_turn_action, pickup(_)]):-
 	debug_term(info, 'Updated agent list: ', VerA).
 
 % Si el predicado member falla, es decir, nunca vimos a este agente:
-remember_agent(Name, [previous_turn_action, pickup(_)]):- 
+remember_agent(Name, [previous_turn_action, pickup(_)]):-
 	debug(info, 'remember_agent: Case D: The agent is seeing picking a treasure but we don´t know it.'),
 	agentes(A),
 	debug_term(info, 'Known agents: ', A),
@@ -126,7 +134,7 @@ remember_agent(Name, [previous_turn_action, pickup(_)]):-
 	debug_term(info, 'Updated agent list: ', VerA).
 
 % Si el agente no hizo nada y ya lo conocemos
-remember_agent(Name, [previous_turn_action, none]):- 
+remember_agent(Name, [previous_turn_action, none]):-
 	debug(info, 'remember_agent: Case E: The agent is evidently a slow one.'),
 	agentes(A),
 	debug_term(info, 'Known agents: ', A),
@@ -138,14 +146,14 @@ remember_agent(Name, [previous_turn_action, none]):-
 	debug_term(info, 'Updated agent list: ', VerA).
 
 % Si el agente no hizo nada y no lo conocimos
-remember_agent(Name, [previous_turn_action, none]):- 
+remember_agent(Name, [previous_turn_action, none]):-
 	debug(info, 'remember_agent: Case F: The agent is evidently a slow one.'),
 	insert_agent(Name, 0, 0, true),
 	agentes(VerA), % Consultamos que todo se haya guardado bien
 	debug_term(info, 'Updated agent list: ', VerA).
 
 % Si el agente no hizo nada
-remember_agent(_Name, [Attr, Val]):- 
+remember_agent(_Name, [Attr, Val]):-
 	debug(warning, 'remember_agent: Case G: What the hell is this?'),
 	term_to_atom(Attr, A),
 	term_to_atom(Val, V),
@@ -154,13 +162,13 @@ remember_agent(_Name, [Attr, Val]):-
 	debug(warning, Str2).
 
 % Inserta a un agente en la lista
-% Caso especial: 
+% Caso especial:
 %	- Cuando el agente tiene como previous_turn_action= none trata de agregar
 %	al agente con 0,0, por eso se checkea not(member(agente(Name, _, _)
-insert_agent(Name, Attack, Picking, Slow):- 
-	agentes(A), 
-	not(member(agente(Name, _, _, _), A)), 
-	replace(agentes(_), 
+insert_agent(Name, Attack, Picking, Slow):-
+	agentes(A),
+	not(member(agente(Name, _, _, _), A)),
+	replace(agentes(_),
 	agentes([agente(Name, Attack, Picking, Slow) | A])).
 
 % Caso especial que el agente ya este insertado, no se hace nada.
@@ -178,6 +186,6 @@ agent_priority(agente(_Name, Attack, Pick, true), Priority):-
 	Priority is Temp * 2.
 
 % Guardo el numero de turno actual
-save_turn(Turn):- 
-	retract(turn(_)), 
+save_turn(Turn):-
+	retract(turn(_)),
 	assert(turn(Turn)).
