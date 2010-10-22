402323381bc064a9744c2b421ad35322b9bfc5d9
unknown <dgm@.(none)>
2010-10-22 18:16:48 -0300

    Implementacion parcial de la estrategia de exploracion.

--- src/Agents/bugorLib/behave.pl
+++ src/Agents/bugorLib/behave.pl
@@ -17,29 +17,36 @@ push_action(Action):- planning_stack(X), replace(planning_stack(X), planning_sta
 decide_action(Action):- 
 	current_strategy(explore),
 	planning_stack([]),
-	current_pos([X, Y]),
-	direction(D),
-	NX is X - 3,
-	NY is Y + 2,
-	assert(meta([NX, NY])),
-	debug_term(warning, 'Meta: ', meta([NX, NY])),
-	debug_term(warning, 'Pos: ', [X, Y]),
-%   debug_term(error, 'Path: ', Path),
-%   debug_term(error, 'Actions: ', NAcs),
-	justdoit([node([X, Y], 0, [])], _,_),
-	read(_),
-	current_action(Action),
+	% current_pos([X, Y]),
+	% direction(D),
+	% NX is X - 3,
+	% NY is Y + 2,
+	% assert(meta([NX, NY])),
+	% debug_term(warning, 'Meta: ', meta([NX, NY])),
+	% debug_term(warning, 'Pos: ', [X, Y]),
+	% debug_term(error, 'Path: ', Path),
+	% debug_term(error, 'Actions: ', NAcs),
+	% justdoit([X, Y], _,_),
+	% read(_),
+	% current_action(Action),
+	explore_strat,
+	debug(warning, 'Termine de explorar'),
+	current_action(Action), 
+	debug_term(warning, 'Ahora voy a ', Action),
 	pop_action.
-%   explore_strat(Action).
 
 decide_action(Action):-
 	current_strategy(explore),
 	current_action(Action),
+	debug_term(warning, 'No hice A*. Ahora voy a ', Action),
 	pop_action.
 
+% init es la lista [X,Y] con la posicion inicial; en RPath y Cost se devuelve el path invertido y el costo
+% justdoit va hacia el nodo marcado por el predicado meta/1
 justdoit(Init, RPath, Cost):-
-	search(Init, Path, Cost),
+	search(node(Init,0,[]), Path, Cost),
 	reverse(Path, [], RPath),
+	direction(D),
 	translateAll(RPath, D),
 	planning_stack(Acs),
 	reverse(Acs, [], NAcs),
--- src/Agents/bugorLib/strategies/general.pl
+++ src/Agents/bugorLib/strategies/general.pl
@@ -1,13 +1,23 @@
-explore_strat(move_fwd):-
+explore_strat:-
 	current_pos([X,Y]),
 	direction(Dir),
-	ady_at_cardinal([X,Y], Dir, [NX, NY]),
-	(map(NX, NY, plain); map(NX, NY, mountain)).
-
-explore_strat(turn(Turn)):-
+	debug_term(warning, 'Current pos = ', [X,Y]),
+	getFrontierPos(Target),
+	replace(meta(_), meta(Target)),
+	debug_term(warning, 'Meta: ', meta(Target)),
+	justdoit([X,Y],_,_),
+	debug(warning, 'Pase el A*.... LETS A-GO!').
+	
+explore_strat:-
 	direction(Dir),
 	random(0, 2, Num),
-	next_random(Num, Dir, Turn).
+	next_random(Num, Dir, Turn),
+	replace(planning_stack(_), planning_stack([])),
+	push_action(turn(Turn)).
+	
+explore_strat:-
+	replace(planning_stack(_), planning_stack([])),
+	push_action(move_fwd).
 
 next_random(0, Dir, Turn):-
 	next_90_clockwise(Dir, Turn).
@@ -15,4 +25,46 @@ next_random(0, Dir, Turn):-
 next_random(1, Dir, Turn):-
 	next_90_clockwise(Dir, Dir1),
 	next_90_clockwise(Dir1, Dir2),
-	next_90_clockwise(Dir2, Turn).
+	next_90_clockwise(Dir2, Turn).	
+
+getFrontierPos(Target):-
+	findall([X,Y], 
+			(	map(X,Y,Land),
+				(
+					Land = mountain 
+					;
+					Land = plain
+				), 
+				P1 is X+1, 
+				P2 is X-1, 
+				P3 is Y+1, 
+				P4 is Y-1,
+				(
+					(not(map(P1,Y, _))) 
+					; 
+					(not(map(P2,Y, _))) 
+					; 
+					(not(map(X, P3, _))) 
+					; 
+					(not(map(X, P4, _)))
+				)
+			),
+			TargetList),
+	debug(warning, 'Pase'),
+	length(TargetList, CantTargets),
+	random(1, CantTargets, Index),
+	nth1(Index, TargetList, Target).
+	
+	
+% explore_strat(turn(Turn)):-
+	% direction(Dir),
+	% random(0, 2, Num),
+	% next_random(Num, Dir, Turn).
+
+% next_random(0, Dir, Turn):-
+	% next_90_clockwise(Dir, Turn).
+
+% next_random(1, Dir, Turn):-
+	% next_90_clockwise(Dir, Dir1),
+	% next_90_clockwise(Dir1, Dir2),
+	% next_90_clockwise(Dir2, Turn).
